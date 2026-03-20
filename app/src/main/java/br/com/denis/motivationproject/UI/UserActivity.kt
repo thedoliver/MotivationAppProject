cc7bbf99342476a1eb7208758dcb18b29641ff52
thedoliver <thedoliver_mach@hotmail.com>
2026-03-20 09:23:13 -0300

    feat: verifyUserName method

--- app/src/main/java/br/com/denis/motivationproject/UI/UserActivity.kt
+++ app/src/main/java/br/com/denis/motivationproject/UI/UserActivity.kt
@@ -1,11 +1,14 @@
 package br.com.denis.motivationproject.UI
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import br.com.denis.motivationproject.helpers.MotivationConstants
 import br.com.denis.motivationproject.R
 import br.com.denis.motivationproject.databinding.ActivityUserBinding
 import br.com.denis.motivationproject.helpers.SecurityPreferences
@@ -13,9 +16,11 @@ import br.com.denis.motivationproject.helpers.SecurityPreferences
 class UserActivity : AppCompatActivity(), View.OnClickListener {
 
     private lateinit var binding: ActivityUserBinding
+    private lateinit var securityPreferences: SecurityPreferences
 
-
-
+    /*
+    Ele faz com que a classe vire uma activity
+     */
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -23,12 +28,17 @@ class UserActivity : AppCompatActivity(), View.OnClickListener {
         binding = ActivityUserBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        securityPreferences = SecurityPreferences(this)
+
+
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
         setListeners()
+        verifyUserName()
 
     }
 
@@ -38,8 +48,27 @@ class UserActivity : AppCompatActivity(), View.OnClickListener {
         }
     }
 
+    private fun verifyUserName(){
+        val nome = securityPreferences.getString(MotivationConstants.KEY.PERSON_NAME)
+        if (nome.isNotEmpty()){
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        }
+
+    }
+
     private fun handleSave(){
-        SecurityPreferences(applicationContext)
+        val name = binding.edittextName.text.toString()
+
+        if (name.isEmpty()){
+            Toast.makeText(this, "Informe seu nome", Toast.LENGTH_SHORT).show()
+        }else {
+            SecurityPreferences(this).storeString(MotivationConstants.KEY.PERSON_NAME, name)
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+
 
     }
 
