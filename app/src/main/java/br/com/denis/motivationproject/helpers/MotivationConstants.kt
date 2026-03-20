aab0382b59cdf0eee00abb732bf16f058091af7c
thedoliver <thedoliver_mach@hotmail.com>
2026-03-20 09:22:19 -0300

    feat: create constanted for language functionality

---
+++ app/src/main/java/br/com/denis/motivationproject/helpers/MotivationConstants.kt
@@ -0,0 +1,21 @@
+package br.com.denis.motivationproject.helpers
+
+class MotivationConstants private constructor() {
+    object KEY {
+        const val PERSON_NAME = "personName"
+    }
+
+    object PHRASE {
+        const val ALL = 1
+        const val HAPPY = 2
+        const val SUNNY = 3
+    }
+
+    object LANGUAGE {
+        const val PORTUGUESE = "pt"
+        const val DEUTSCH = "de"
+        const val ENGLISH = "en"
+        const val FINNISH = "fi"
+    }
+
+}
