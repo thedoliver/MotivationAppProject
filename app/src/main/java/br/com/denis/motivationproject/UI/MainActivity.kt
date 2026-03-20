e29b9c9d1e6b968f079d738110c1dcb793f4e539
thedoliver <thedoliver_mach@hotmail.com>
2026-03-20 09:21:54 -0300

    feat: security reference implemented

--- app/src/main/java/br/com/denis/motivationproject/UI/MainActivity.kt
+++ app/src/main/java/br/com/denis/motivationproject/UI/MainActivity.kt
@@ -4,10 +4,15 @@ import android.os.Bundle
 import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import br.com.denis.motivationproject.helpers.MotivationConstants
+import br.com.denis.motivationproject.repositories.PhraseRepository
 import br.com.denis.motivationproject.R
 import br.com.denis.motivationproject.databinding.ActivityMainBinding
+import br.com.denis.motivationproject.helpers.SecurityPreferences
+import java.util.Locale
 
 class MainActivity : AppCompatActivity(), View.OnClickListener {
 
@@ -18,6 +23,12 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
      * tipo é Activity
      */
     private lateinit var binding: ActivityMainBinding
+    private lateinit var securityPreferences: SecurityPreferences
+
+    private val phraseRepository = PhraseRepository()
+    private var filter: Int = MotivationConstants.PHRASE.ALL
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -28,6 +39,9 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
          */
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        securityPreferences = SecurityPreferences(this)
+
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
@@ -41,16 +55,58 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
          * em um metodo
          */
         setListeners()
+        getUsername()
+        handleFilter(R.id.image_all)
+        refreshPhrase()
     }
 
+
+
     override fun onClick(v: View) {
+
+        val listId = listOf(R.id.image_all, R.id.image_happy, R.id.image_sunny)
+
         if (v.id == R.id.button_new_phrase){
-            handleNewPhrase()
+            refreshPhrase()
+        }else if (v.id in listId){
+            handleFilter(v.id)
+
+        }
+
+    }
+
+    private fun handleFilter(id: Int) {
+
+        binding.imageAll.setColorFilter(ContextCompat.getColor(this, R.color.black))
+        binding.imageHappy.setColorFilter(ContextCompat.getColor(this, R.color.black))
+        binding.imageSunny.setColorFilter(ContextCompat.getColor(this, R.color.black))
+
+
+
+        when (id){
+            R.id.image_all -> {
+                filter = MotivationConstants.PHRASE.ALL
+                binding.imageAll.setColorFilter(ContextCompat.getColor(this, R.color.white))
+
+            }
+            R.id.image_happy -> {
+                filter = MotivationConstants.PHRASE.HAPPY
+                binding.imageHappy.setColorFilter(ContextCompat.getColor(this, R.color.white))
+            }
+            R.id.image_sunny -> {
+                filter = MotivationConstants.PHRASE.SUNNY
+                binding.imageSunny.setColorFilter(ContextCompat.getColor(this, R.color.white))
+            }
         }
     }
 
-    private fun handleNewPhrase() {
+    private fun refreshPhrase() {
+        binding.textviewPhrase.text = phraseRepository.getPhrase(filter, Locale.getDefault().language)
+    }
 
+    private fun getUsername(){
+        val name = securityPreferences.getString(MotivationConstants.KEY.PERSON_NAME)
+        binding.textviewName.text = name
     }
 
 
@@ -60,7 +116,9 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
          * e sobreescrever o metodo onClick sendo um interface preciso implementar o contrato
          */
         binding.buttonNewPhrase.setOnClickListener(this)
-
+        binding.imageAll.setOnClickListener(this)
+        binding.imageHappy.setOnClickListener(this)
+        binding.imageSunny.setOnClickListener(this)
     }
 
 
